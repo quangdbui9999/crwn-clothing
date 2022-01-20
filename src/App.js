33e826d1e4248c98a6f6024f240515035a2ed35c
Quang Bui <qb37@drexel.edu>
2022-01-20 11:57:26 -0500

    learn linkUrl

--- src/App.js
+++ src/App.js
@@ -1,11 +1,25 @@
 import React from "react";
+import { Switch, Route } from 'react-router-dom';
 import HomePage from "./pages/homepage/homepage.component";
 import "./App.css";
 
+const HatsPage = (props) => {
+  return (
+    <div>
+      <h1>HATS PAGE</h1>
+    </div>
+  );
+};
+
 function App() {
-  return <div>
-    <HomePage />
-  </div>;
+  return (
+    <div className="App">
+      <Switch>
+        <Route exact path='/' component={HomePage} />
+        <Route path='/hats' component={HatsPage} />
+      </Switch>
+    </div>
+  );
 }
 
 export default App;
